a95a81f4676da773ed731cf415c96cb643954a53
Paul Myhr <Goreng@Pauls-MacBook-Pro.local>
2016-11-17 12:18:10 -0800

    add start of interface testing and output

---
+++ client/output.c
@@ -0,0 +1,61 @@
+#include <gtk/gtk.h>
+#if defined(__APPLE__)
+	#include <OpenGL/gl.h>
+	#include <OpenGL/glu.h>
+	#include <GLUT/glut.h>
+#else
+	#include <GL/gl.h>
+	#include <GL/glu.h>
+	#include <freeglut.h>
+#endif
+
+extern int call_crystal_(char[40]);
+
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  char filename [40] = "../data/thymine_rho.cube";
+  gchar *display;
+  int num = call_crystal_(filename);
+  display = g_strdup_printf("%d", num);
+  g_print (display);
+}
+
+static void
+activate (GtkApplication *app,
+          gpointer        user_data)
+{
+  GtkWidget *window;
+  GtkWidget *button;
+  GtkWidget *button_box;
+
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+
+  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
+  gtk_container_add (GTK_CONTAINER (window), button_box);
+
+  button = gtk_button_new_with_label ("Hello World");
+  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
+  // g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+  gtk_container_add (GTK_CONTAINER (button_box), button);
+
+  gtk_widget_show_all (window);
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
---
+++ src/interface.f90
@@ -0,0 +1,57 @@
+module interface
+  use tricks
+  use stm
+  use xdm
+  use ewald
+  use hirshfeld
+  use qtree
+  use bisect
+  use integration
+  use flux
+  use autocp
+  use nci
+  use rhoplot
+  use fields
+  use varbas
+  use grd_atomic
+  use struct
+  use struct_basic
+  use wfn_private
+  use pi_private
+  use spgs
+  use global
+  use config
+  use graphics
+  use arithmetic
+  use types
+  use tools
+  use tools_io
+  use param
+
+  private
+
+  public :: call_crystal
+
+contains
+  !xx! top-level routines
+
+  subroutine call_crystal(filename) bind ( C, name="call_crystal" )
+
+    use iso_c_binding, only: C_CHAR, c_null_char
+    implicit none
+
+    character (kind=c_char, len=1), dimension (40), intent (in) :: filename
+
+    if (cr%isinit) call clean_structure()
+    ! read the crystal environment
+    call struct_crystal_input(cr,filename,.false.,.true.,.true.)
+    if (cr%isinit) then
+       ! initialize the radial densities
+       call grda_init(.true.,.true.,.true.)
+       ! set the promolecular density as reference
+       call set_reference(0)
+    else
+       call cr%init()
+    end if
+
+  end subroutine call_crystal
