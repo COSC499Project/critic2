0d569847fd8e3b161d2f2a6131485a38f4fd7f20
Paul Myhr <Goreng@Pauls-MacBook-Pro.local>
2017-02-05 13:32:40 -0800

    move interface folder into source and compile properly

--- interface/compile.sh
+++
@@ -1,2 +0,0 @@
-gfortran -c interface.f90 -I../src
-g++ interface.o crystal.cpp -o crystal ../src/criticlib.a -lgfortran ../src/qhull/libqhull_critic.a ../src/cubpack/libcubpack.a ../src/ciftbx/libciftbx.a ../src/oldlibs/libmisc.a -lgomp
