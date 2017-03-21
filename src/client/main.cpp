2175e23c6ca6de3666b1b9fc1abb6a56179c02a0
Paul Myhr <Goreng@Pauls-MacBook-Pro.local>
2017-03-21 15:44:17 -0700

    changes to get positions

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -11,18 +11,12 @@
 #include "matrix_math.cpp"
 #include "tinyfiledialogs.h"
 
-#ifdef WINDOWS
-  #include <direct.h>
-  #define GetCurrentDir _getcwd
-#else
-  #include <unistd.h>
-  #define GetCurrentDir getcwd
-#endif
-
 extern "C" void initialize();
 extern "C" void init_struct();
 extern "C" void call_structure(const char *filename, int size, int isMolecule);
-extern "C" void get_positions(int *n,int **z,double **x);
+// extern "C" void get_positions(int *n,int **z,double **x);
+extern "C" void get_num_atoms(int *n);
+extern "C" void get_atom_position(int n, int *atomicN, double *x, double *y, double *z);
 //extern "C" void get_atomic_name(const char *atomName, int atomNum);
 extern "C" void share_bond(int n_atom, int **connected_atom);
 
@@ -315,36 +309,27 @@ void deselectAll() {
 //the atoms should be loaded into the above array
 void loadAtoms() {
   //fill loadedAtoms array
-  int *z; // atomic numbers
-  double *x; // atomic positions
-  int n; // number of atoms
-
-  get_positions(&n,&z,&x);
+  int n;
+  get_num_atoms(&n);
+  printf("num of atoms %d\n", n);
 
   loadedAtomsAmount = n;
 	loadedAtoms = new atom[loadedAtomsAmount];
-  for (int i=0;i<n;i++) {
-    // share_bond(i, &connected_atoms);
-    loadedAtoms[i].atomicNumber = z[i];
-    loadedAtoms[i].atomPosition[0] = x[i*3+0];
-  	loadedAtoms[i].atomPosition[1] = x[i*3+1];
-  	loadedAtoms[i].atomPosition[2] = x[i*3+2];
+  for (int i=0; i < n; i++) {
+    int atomicN;
+    double x;
+    double y;
+    double z;
 
-    // loadedAtoms[i].bondedAtoms = connected_atoms;
-    // int *connected_atoms;
-    // int size = sizeof(loadedAtoms[i].bondedAtoms)
-    //
-    // printf(loadedAtoms[i].bondedAtoms);
+    get_atom_position(i+1, &atomicN, &x, &y, &z);
+    printf("Atoms: %d %d %.10f %.10f %.10f\n",i+1,atomicN, x, y, z);
+
+    loadedAtoms[i].atomicNumber = atomicN;
+    loadedAtoms[i].atomPosition[0] = x;
+  	loadedAtoms[i].atomPosition[1] = y;
+  	loadedAtoms[i].atomPosition[2] = z;
   }
 
-  // for (int i=0; i<n; i++) {
-  //
-  // }
-
-	// loadedAtoms[0].atomicNumber = 1;
-	// loadedAtoms[0].atomPosition[0] = 0.f;
-	// loadedAtoms[0].atomPosition[1] = -1.f;
-	// loadedAtoms[0].atomPosition[2] = 0.f;
   //tree names must be constant
 	for (size_t x = 0; x < loadedAtomsAmount; x++) {
 		std::string nodeName = "";
@@ -766,7 +751,7 @@ int main(int, char**)
 
         //static float p1[3] = {-1, 0, 0};
         //static float p2[3] = {1, 0, 0};
-        drawAllBonds(&p, CylVB, CylIB, SphereVB, SphereIB);
+        // drawAllBonds(&p, CylVB, CylIB, SphereVB, SphereIB);
         //DrawBondLighted(&p, CylVB, CylIB, p1, p2, SphereVB, SphereIB);
 
 
@@ -820,7 +805,7 @@ static void ShowMenuFile()
       init_struct();
       call_structure(lTheOpenFileName, (int) strlen(lTheOpenFileName), 1);
       loadAtoms();
-      loadBonds();
+      // loadBonds();
     }
     if (ImGui::MenuItem("Crystal")) {
       char const * lTheOpenFileName = tinyfd_openFileDialog(
